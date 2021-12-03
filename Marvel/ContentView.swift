07c115b652a38b531d626992bca204ba7226b120
Adrien <adrnsmn@gmail.com>
2021-12-03 18:13:46 +0100

    Initial Commit

---
+++ Marvel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Marvel
+//
+//  Created by Admin on 03/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Marvel/MarvelApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelApp.swift
+//  Marvel
+//
+//  Created by Admin on 03/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct MarvelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
